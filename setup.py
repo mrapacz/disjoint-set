fa3ac22797719474fb41caa806d2664259e6218a
Maciej Rapacz <maciek.rapacz@gmail.com>
2019-05-24 00:16:33 +0200

    Bump package version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
     name='disjoint_set',
-    version='0.0.2',
+    version='0.0.3',
     packages=setuptools.find_packages(),
     description='Disjoint set data structure implementation for Python',
     url='https://github.com/mrapacz/disjoint_set',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     maintainer="Maciej Rapacz",
     maintainer_email="python.disjointset@gmail.com",
     classifiers=[
