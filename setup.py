28d1f152bc45c52662e63ead7f9c8f52786c8f63
Maciej Rapacz <maciek.rapacz@gmail.com>
2019-05-23 22:16:36 +0200

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='disjoint_set',
+    version='0.1',
+    packages=['disjoint_set', ],
+    license='MIT',
+    description='Disjoint set data structure implementation for Python',
+    url='https://github.com/mrapacz/disjoint_set',
+    long_description=open('README.txt').read(),
+)
