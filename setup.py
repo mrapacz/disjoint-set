be6e9c87f5e4dab8e6155c3bd9a8f9ce8b0f7408
Maciej Rapacz <maciek.rapacz@gmail.com>
2019-05-23 23:37:42 +0200

    Bump package version

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='disjoint_set',
-    version='0.1',
-    packages=['disjoint_set', ],
-    license='MIT',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
     description='Disjoint set data structure implementation for Python',
     url='https://github.com/mrapacz/disjoint_set',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    maintainer="Maciej Rapacz",
+    maintainer_email="python.disjointset@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
