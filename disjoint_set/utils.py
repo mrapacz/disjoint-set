eb86de19b03daa35dc69e5847ce2a04451d03e4f
Maciej Rapacz <mmrapacz@protonmail.ch>
2020-08-07 21:50:44 +0200

    Allow for deep copying of the data structure

--- disjoint_set/utils.py
+++ disjoint_set/utils.py
@@ -5,16 +5,16 @@ VT = TypeVar('VT')
 T = TypeVar('T')
 
 
+def identity(x: T) -> T:
+    return x
+
+
 class ArgDefaultDict(DefaultDict[KT, VT]):
-    def __init__(self, fun_with_param: Callable[[KT], VT]):
-        super().__init__(None)
+    def __init__(self, fun_with_param: Callable[[KT], VT] = identity, *args, **kwargs):
+        super().__init__(None, *args, **kwargs)
         self.fun: Callable[[KT], VT] = fun_with_param
 
     def __missing__(self, key: KT) -> VT:
         ret: VT = self.fun(key)
         self[key] = ret
         return ret
-
-
-def identity(x: T) -> T:
-    return x
