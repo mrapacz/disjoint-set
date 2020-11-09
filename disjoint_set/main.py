9590198829436062da855f6e7f39169d7aa9280c
Maciej Rapacz <mmrapacz@protonmail.ch>
2020-11-09 22:14:12 +0100

    Update docstrings

--- disjoint_set/main.py
+++ disjoint_set/main.py
@@ -5,10 +5,10 @@ from typing import Any
 from typing import DefaultDict
 from typing import Generic
 from typing import Iterator
-from typing import List
 from typing import Set
 from typing import Tuple
 from typing import TypeVar
+from typing import Union
 
 from disjoint_set.utils import IdentityDict
 
@@ -27,6 +27,9 @@ class DisjointSet(Generic[T]):
     def __bool__(self) -> bool:
         return bool(self._data)
 
+    def __get__(self, element: T) -> T:
+        return self.find(element)
+
     def __eq__(self, other: Any) -> bool:
         """
         Return True if both DistjoinSet structures are equivalent.
@@ -41,38 +44,55 @@ class DisjointSet(Generic[T]):
         return {tuple(x) for x in self.itersets()} == {tuple(x) for x in other.itersets()}
 
     def __repr__(self) -> str:
-        value_dict: DefaultDict[T, List[T]] = defaultdict(list)
-        for key, value in self._data.items():
-            value_dict[value].append(key)
-        return f"{self.__class__.__name__}({dict(self)!r})"
+        """
+        Print self in a reproducible way.
+
+        >>> DisjointSet({1: 2, 2: 2})
+        DisjointSet({1: 2, 2: 2})
+        """
+        sets = {key: val for key, val in self}
+        return f"{self.__class__.__name__}({sets})"
+
+    def __str__(self) -> str:
+        return "{classname}({values})".format(
+            classname=self.__class__.__name__, values=", ".join(str(dset) for dset in self.itersets()),
+        )
 
     def __iter__(self) -> Iterator[Tuple[T, T]]:
+        """Iterate over items and their canonical elements."""
         for key in self._data:
             yield key, self.find(key)
 
-    def itersets(self) -> Iterator[Set[T]]:
+    def itersets(self, with_canonical_elements: bool = False) -> Iterator[Union[Set[T], Tuple[T, Set[T]]]]:
         """
         Yield sets of connected components.
 
+        If with_canonical_elements is set to True, method will yield tuples of (<canonical_element>, <set of elements>)
         >>> ds = DisjointSet()
         >>> ds.union(1,2)
         >>> list(ds.itersets())
         [{1, 2}]
+        >>> list(ds.itersets(with_canonical_elements=True))
+        [(2, {1, 2})]
         """
-        element_classes: DefaultDict[T, Set[T]] = defaultdict(lambda: set())
+        element_classes: DefaultDict[T, Set[T]] = defaultdict(set)
         for element in self._data:
             element_classes[self.find(element)].add(element)
 
-        yield from element_classes.values()
+        if with_canonical_elements:
+            yield from element_classes.items()
+        else:
+            yield from element_classes.values()
 
     def find(self, x: T) -> T:
         """
-        Return the representative member of the set of connected components to which x belongs, may be x itself.
+        Return the canonical element of a given item.
 
+        In case the element was not present in the data structure, the canonical element is the item itself.
         >>> ds = DisjointSet()
         >>> ds.find(1)
         1
-        >>> ds.union(1,2)
+        >>> ds.union(1, 2)
         >>> ds.find(1)
         2
         """
@@ -96,10 +116,10 @@ class DisjointSet(Generic[T]):
         Return True if x and y belong to the same set (i.e. they have the canonical element).
 
         >>> ds = DisjointSet()
-        >>> ds.connected(1,2)
+        >>> ds.connected(1, 2)
         False
-        >>> ds.union(1,2)
-        >>> ds.connected(1,2)
+        >>> ds.union(1, 2)
+        >>> ds.connected(1, 2)
         True
         """
         return self.find(x) == self.find(y)
